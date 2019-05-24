5c4b9d02f82f54c8cc1ac74f94fd5a6cfc3e2c48
Alexander Lindholm (DTK) <Alexander.DTK.Lindholm@lystkom.se>
2019-05-24 13:28:38 +0200

    Add project files.

---
+++ Polymorfism/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Polymorfism
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbA = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSkapa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rbA
+            // 
+            this.rbA.AutoSize = true;
+            this.rbA.Location = new System.Drawing.Point(31, 61);
+            this.rbA.Name = "rbA";
+            this.rbA.Size = new System.Drawing.Size(126, 24);
+            this.rbA.TabIndex = 0;
+            this.rbA.TabStop = true;
+            this.rbA.Text = "radioButton1";
+            this.rbA.UseVisualStyleBackColor = true;
+            this.rbA.CheckedChanged += new System.EventHandler(this.rbA_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(31, 115);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(126, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 263);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(303, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnSkapa
+            // 
+            this.btnSkapa.Location = new System.Drawing.Point(259, 169);
+            this.btnSkapa.Name = "btnSkapa";
+            this.btnSkapa.Size = new System.Drawing.Size(75, 40);
+            this.btnSkapa.TabIndex = 3;
+            this.btnSkapa.Text = "Skapa";
+            this.btnSkapa.UseVisualStyleBackColor = true;
+            this.btnSkapa.Click += new System.EventHandler(this.btnSkapa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 305);
+            this.Controls.Add(this.btnSkapa);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.rbA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbA;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSkapa;
+    }
+}
+
---
+++ Polymorfism/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Polymorfism
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSkapa_Click(object sender, EventArgs e)
+        {
+            A obj;
+
+            if (rbA.Checked)
+            {
+                obj = new A();
+            }
+            else
+            {
+                obj = new B();
+            }
+
+            if (obj is B)
+            {
+                B bObj = (obj as B);
+                textBox1.Text = bObj.Description();
+            }
+            else
+            {
+                textBox1.Text = obj.Description();
+            }
+        }
+
+        private void rbA_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
